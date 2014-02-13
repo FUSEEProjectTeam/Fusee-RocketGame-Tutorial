9cb4d1fe50b0b2d33321c01fd85f0fab12883338
Alexander Scheurer <mail@aspepex.net>
2014-02-14 00:36:08 +0100

    Lesson 10 done

---
+++ projects/Lesson10/GameWorld.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using Fusee.Engine;
+using Fusee.Math;
+
+namespace Examples.RocketGame.Lesson10
+{
+    public class GameWorld
+    {
+        private readonly RenderContext _rc;
+
+        private readonly List<GameEntity> _furniture = new List<GameEntity>();
+        private readonly List<Target> _targets = new List<Target>();
+
+        private readonly Player _player;
+
+        private readonly GUI _gui;
+
+        private float4x4 _camMatrix;
+
+        private int _currentGameState;
+        private int _lastGameState;
+
+        private int _curScore;
+        private int _maxScore;
+        private int _oldScore;
+
+        public GameWorld(RenderContext rc)
+        {
+            _rc = rc;
+
+            _currentGameState = (int) GameState.StartScreen;
+
+            _gui = new GUI(rc, this);
+
+            _player = new Player("Assets/cube.obj.model", _rc);
+
+            _furniture.Add(new GameEntity("Assets/cube.obj.model", _rc, -100, 0, -500));
+            _furniture[0].SetShader(new float4(1, 0, 0, 1));
+            _furniture[0].SetCorrectionMatrix(float4x4.Scale(0.5f));
+
+            _furniture.Add(new GameEntity("Assets/cube.obj.model", _rc, 100, 0, -500, (float) Math.PI/4, (float) Math.PI/4));
+            _furniture[1].SetShader(new float4(0, 1, 0, 1), "Assets/toon_generic_5_tex.png", new float4(0, 0, 0, 1), new float2(10, 10));
+
+            _targets.Add(new Target("Assets/cube.obj.model", rc, 0, 0, -1000, (float) Math.PI/4, (float) Math.PI/4, (float) Math.PI/4));
+            _targets[0].SetCorrectionMatrix(float4x4.Scale(0.5f));
+            _targets.Add(new Target("Assets/cube.obj.model", rc, 0, 0, -3000, (float) Math.PI/4, (float) Math.PI/4, (float) Math.PI/4));
+            _targets[1].SetCorrectionMatrix(float4x4.Scale(0.5f));
+
+            _maxScore = _targets.Count;
+        }
+
+        public void RenderAFrame()
+        {
+            if (_currentGameState != _lastGameState)
+            {
+                switch (_currentGameState)
+                {
+                    case (int)GameState.StartScreen:
+                        _camMatrix = float4x4.CreateTranslation(0, 0, 0);
+                        _gui.ShowStartGUI();
+                        break;
+
+                    case (int)GameState.Running:
+                        _gui.ShowPlayGUI();
+                        _player.SetPosition(float4x4.Identity);
+                        break;
+
+                    case (int)GameState.GameOver:
+                        foreach (var target in _targets)
+                        {
+                            target.SetInactive();
+                        }
+                        _gui.ShowStartGUI();
+                        _gui.ShowOverGUI();
+                        _camMatrix = float4x4.CreateTranslation(0, 0, 0);
+                        _player.Speed = 0;
+                        break;
+                }
+                _lastGameState = _currentGameState;
+            }
+            
+            if (_currentGameState == (int) GameState.Running)
+            {
+                _player.Move();
+                _camMatrix = _player.GetCamMatrix();
+                _player.Render(_camMatrix);
+
+                int activeGoalCount = 0;
+                foreach (var target in _targets)
+                {
+                    var distanceVector = _player.GetPositionVector() - target.GetPositionVector();
+                    if (distanceVector.Length < 500)
+                    {
+                        target.SetActive();
+                    }
+                    if (target.GetStatus())
+                    {
+                        activeGoalCount++;
+                    }
+                    target.Render(_camMatrix);
+                }
+                _curScore = activeGoalCount;
+
+                if (_curScore != _oldScore)
+                {
+                    _gui.UpdateScore();
+                }
+
+                _oldScore = _curScore;
+
+                if (_curScore == _maxScore)
+                {
+                    _currentGameState = (int) GameState.GameOver;
+                }
+
+            }
+            else
+            {
+                _camMatrix *= float4x4.CreateRotationY(0.02f);
+            }
+            
+            foreach (var gameEntity in _furniture)
+            {
+                gameEntity.Render(_camMatrix);
+            }
+
+            _gui.Render();
+        }
+
+        public void Resize()
+        {
+            _gui.Resize();
+        }
+
+        public void SetGamestate(int gameState)
+        {
+            _currentGameState = gameState;
+        }
+
+        public int GetScore()
+        {
+            return _curScore;
+        }
+
+        public int GetMaxScore()
+        {
+            return _maxScore;
+        }
+    }
+
+    public enum GameState
+    {
+        StartScreen,
+        Running,
+        GameOver
+    }
+}
---
+++ projects/Lesson10/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Fusee.Engine;
+using Fusee.Math;
+
+namespace Examples.RocketGame.Lesson10
+{
+    public class Player : GameEntity
+    {
+        private float3 _rotation;
+        private float3 _rotationSpeed = new float3(1, 1, 1);
+        private float _speed;
+        private float _speedModifier = 1;
+        private float SpeedMax = 30f;
+
+        private float3 _oldPos;
+
+        private float3 _nRotXV;
+        private float3 _nRotYV;
+        private float3 _nRotZV;
+
+        public Player(String meshPath, RenderContext rc, float posX = 0, float posY = 0, float posZ = 0, float angX = 0, float angY = 0, float angZ = 0) : base(meshPath, rc, posX, posY, posZ, angX, angY, angZ)
+        {
+            UpdateNRVectors();
+        }
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public void Move()
+        {
+            if (Input.Instance.IsKey(KeyCodes.D))
+                _rotation.x = _rotationSpeed.x * (float) Time.Instance.DeltaTime;
+            else if (Input.Instance.IsKey(KeyCodes.A))
+                _rotation.x = -_rotationSpeed.x * (float)Time.Instance.DeltaTime;
+            else
+                _rotation.x = 0;
+
+            if (Input.Instance.IsKey(KeyCodes.W))
+                _rotation.y = _rotationSpeed.y * (float)Time.Instance.DeltaTime;
+            else if (Input.Instance.IsKey(KeyCodes.S))
+                _rotation.y = -_rotationSpeed.x * (float)Time.Instance.DeltaTime;
+            else
+                _rotation.y = 0;
+
+            if (Input.Instance.IsKey(KeyCodes.E))
+                _rotation.z = _rotationSpeed.z * (float)Time.Instance.DeltaTime;
+            else if (Input.Instance.IsKey(KeyCodes.Q))
+                _rotation.z = -_rotationSpeed.z * (float)Time.Instance.DeltaTime;
+            else
+                _rotation.z = 0;
+
+            if (Input.Instance.IsKey(KeyCodes.Up) || Input.Instance.IsKey(KeyCodes.Space))
+            {
+                _speed += _speedModifier * (float)Time.Instance.DeltaTime;
+            }
+            else
+            {
+                _speed -= _speedModifier * (float)Time.Instance.DeltaTime;
+            }
+
+            _speed = Clamp(_speed, 0.0f, SpeedMax);
+
+            
+            _oldPos.x = Position.Row3.x;
+            _oldPos.y = Position.Row3.y;
+            _oldPos.z = Position.Row3.z;
+
+            Position *= float4x4.CreateTranslation(-_oldPos) *
+                        float4x4.CreateFromAxisAngle(_nRotYV, -_rotation.x) *
+                        float4x4.CreateFromAxisAngle(_nRotXV, -_rotation.y) *
+                        float4x4.CreateFromAxisAngle(_nRotZV, -_rotation.z) *
+                        float4x4.CreateTranslation(_oldPos) *
+                        float4x4.CreateTranslation(_nRotZV * -_speed);
+
+            UpdateNRVectors();
+        }
+
+        public float4x4 GetCamMatrix()
+        {
+            return float4x4.LookAt(Position.M41 + (_nRotZV.x * 1000),
+                                   Position.M42 + (_nRotZV.y * 1000),
+                                   Position.M43 + (_nRotZV.z * 1000),
+                                   Position.M41,
+                                   Position.M42,
+                                   Position.M43,
+                                   Position.M21,
+                                   Position.M22,
+                                   Position.M23)
+                                   * float4x4.CreateTranslation(0, -300, 0);
+        }
+
+        private static float Clamp(float value, float min, float max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        protected void UpdateNRVectors()
+        {
+            _nRotXV = float3.Normalize(new float3(Position.Row0));
+            _nRotYV = float3.Normalize(new float3(Position.Row1));
+            _nRotZV = float3.Normalize(new float3(Position.Row2));
+        }
+
+    }
+}
