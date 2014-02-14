a912f6f1c142e9deefa14af8c165c84df3936316
Alexander Scheurer <mail@aspepex.net>
2014-02-14 16:17:30 +0100

    Another update for Player.cs

--- projects/Lesson08/Player.cs
+++ projects/Lesson08/Player.cs
@@ -12,15 +12,9 @@ namespace Examples.RocketGame.Lesson08
         private float _speedModifier = 1;
         private float SpeedMax = 30f;
 
-        private float3 _oldPos;
-
-        private float3 _nRotXV;
-        private float3 _nRotYV;
-        private float3 _nRotZV;
-
-        public Player(String meshPath, RenderContext rc, float posX = 0, float posY = 0, float posZ = 0, float angX = 0, float angY = 0, float angZ = 0) : base(meshPath, rc, posX, posY, posZ, angX, angY, angZ)
+        public Player(String meshPath, RenderContext rc, float posX = 0, float posY = 0, float posZ = 0, float angX = 0, float angY = 0, float angZ = 0)
+            : base(meshPath, rc, posX, posY, posZ, angX, angY, angZ)
         {
-            UpdateNRVectors();
         }
 
         public float Speed
@@ -32,7 +26,7 @@ namespace Examples.RocketGame.Lesson08
         public void Move()
         {
             if (Input.Instance.IsKey(KeyCodes.D))
-                _rotation.x = _rotationSpeed.x * (float) Time.Instance.DeltaTime;
+                _rotation.x = _rotationSpeed.x * (float)Time.Instance.DeltaTime;
             else if (Input.Instance.IsKey(KeyCodes.A))
                 _rotation.x = -_rotationSpeed.x * (float)Time.Instance.DeltaTime;
             else
@@ -63,46 +57,31 @@ namespace Examples.RocketGame.Lesson08
 
             _speed = Clamp(_speed, 0.0f, SpeedMax);
 
-            
-            _oldPos.x = Position.Row3.x;
-            _oldPos.y = Position.Row3.y;
-            _oldPos.z = Position.Row3.z;
-
-            Position *= float4x4.CreateTranslation(-_oldPos) *
-                        float4x4.CreateFromAxisAngle(_nRotYV, -_rotation.x) *
-                        float4x4.CreateFromAxisAngle(_nRotXV, -_rotation.y) *
-                        float4x4.CreateFromAxisAngle(_nRotZV, -_rotation.z) *
-                        float4x4.CreateTranslation(_oldPos) *
-                        float4x4.CreateTranslation(_nRotZV * -_speed);
-
-            UpdateNRVectors();
+            Position *= float4x4.CreateTranslation(-Position.Row3.xyz) *
+                        float4x4.CreateFromAxisAngle(float3.Normalize(Position.Row1.xyz), -_rotation.x) *
+                        float4x4.CreateFromAxisAngle(float3.Normalize(Position.Row0.xyz), -_rotation.y) *
+                        float4x4.CreateFromAxisAngle(float3.Normalize(Position.Row2.xyz), -_rotation.z) *
+                        float4x4.CreateTranslation(Position.Row3.xyz) *
+                        float4x4.CreateTranslation(float3.Normalize(Position.Row2.xyz) * -_speed);
         }
 
         public float4x4 GetCamMatrix()
         {
-            return float4x4.LookAt(Position.M41 + (_nRotZV.x * 1000),
-                                   Position.M42 + (_nRotZV.y * 1000),
-                                   Position.M43 + (_nRotZV.z * 1000),
+            return float4x4.LookAt(Position.M41 + (float3.Normalize(Position.Row2.xyz).x),
+                                   Position.M42 + (float3.Normalize(Position.Row2.xyz).y),
+                                   Position.M43 + (float3.Normalize(Position.Row2.xyz).z),
                                    Position.M41,
                                    Position.M42,
                                    Position.M43,
                                    Position.M21,
                                    Position.M22,
                                    Position.M23)
-                                   * float4x4.CreateTranslation(0, -300, 0);
+                                   * float4x4.CreateTranslation(0, -300, -1000);
         }
 
         private static float Clamp(float value, float min, float max)
         {
             return (value < min) ? min : (value > max) ? max : value;
         }
-
-        private void UpdateNRVectors()
-        {
-            _nRotXV = float3.Normalize(new float3(Position.Row0));
-            _nRotYV = float3.Normalize(new float3(Position.Row1));
-            _nRotZV = float3.Normalize(new float3(Position.Row2));
-        }
-
     }
 }
